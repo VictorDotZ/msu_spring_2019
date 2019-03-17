edcdab9a4736081f7bac018b9958ec1486529a49
Victor Zenin <super.cat3124@gmail.com>
2019-03-17 17:24:36 +0300

    fix: remove new[]/delete[] array

--- 01/test.cpp
+++ 01/test.cpp
@@ -8,26 +8,16 @@ bool isPrimeNumber(int var);
 
 size_t find(const int* data, size_t dataSize, int var);
 
-int amountOfPrimeNumbers(int start, int end, const int* data, size_t dataSize);
+size_t amountOfPrimeNumbers(int start, int end, const int* data, size_t dataSize);
 
 int main(int argc, char* argv[])
 {
     if ((argc % 2 == 0) || (argc == 1))
         return -1;
 
-    // каким именно способом осуществляется подача данных для массива непонятно
-    
-    size_t dataSize = sizeof(Data) / sizeof(Data[0]);
-    int* data = new int[Size];
-    
-    for (size_t i = 0; i < dataSize; i++)
-        data[i] = Data[i];
-    
     for (int i = 2; i < argc; i += 2)
         std::cout << amountOfPrimeNumbers(std::atoi(argv[i-1]), std::atoi(argv[i]),
-        data, dataSize) << std::endl;
-
-    delete[] data;
+        Data, Size) << std::endl;
     
     return 0;
 }
@@ -53,7 +43,7 @@ bool isPrimeNumber(int var)
     return true;
 }
 
-int amountOfPrimeNumbers(int start, int end, const int* data, size_t dataSize)
+size_t amountOfPrimeNumbers(int start, int end, const int* data, size_t dataSize)
 {
     size_t startIndex = find(data, dataSize, start);
     size_t endIndex = find(data, dataSize, end);
@@ -61,7 +51,7 @@ int amountOfPrimeNumbers(int start, int end, const int* data, size_t dataSize)
     if ((startIndex == dataSize) || (endIndex == dataSize))
         return 0;
 
-    int amount = 0;
+    size_t amount = 0;
 
     for (size_t i = startIndex; i <= endIndex; i++)
         if (isPrimeNumber(data[i]))
