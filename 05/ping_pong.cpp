fdfe0c5eaf08c115819bac712b4a2b0d0fc727b1
Victor Zenin <super.cat3124@gmail.com>
2019-04-24 19:27:56 +0300

    v0.1 ping pong

---
+++ 05/ping_pong.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex m;
+std::condition_variable printed;
+bool pingPrinted = false;
+const int N = 500000;
+
+
+void printPing()
+{
+    for (int i = 0; i < N; i++) {
+        std::unique_lock<std::mutex> lock(m);
+        printed.wait(lock, [] { return !pingPrinted; });
+        std::cout << "ping\n";
+        pingPrinted = true;
+        printed.notify_one();
+    }
+}
+
+
+void printPong()
+{
+    for (int i = 0; i < N; i++) {
+        std::unique_lock<std::mutex> lock(m);
+        printed.wait(lock, [] { return pingPrinted; } );
+        std::cout << "pong\n";
+        pingPrinted = false;
+        printed.notify_one();
+    }
+}
+
+
+int main()
+{
+    std::thread ping(printPing);
+    std::thread pong(printPong);
+
+    ping.join();
+    pong.join();
+
+    return 0;
+}
