26bbfa3172b6e47bb533092bbc13f4d02901b562
Victor Zenin <super.cat3124@gmail.com>
2019-05-14 09:41:49 +0300

    replace 'for'

--- 06/main.cpp
+++ 06/main.cpp
@@ -3,16 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <stack>
-
-
-template <class T>
-std::vector<T> takeSequence(const std::vector<T>& array, size_t left,
-                            size_t right) {
-    std::vector<T> result(right - left);
-    for (size_t i = 0; i < result.size(); i++)
-        result[i] = array[left + i];
-    return result;
-}
+#include <cstring>
 
 
 template <class T>
@@ -31,18 +22,9 @@ void merge(std::vector<T>& array, size_t left, size_t mid, size_t right) {
         }
     }
 
-    while (left + it1 < mid) {
-        result[it1 + it2] = array[left + it1];
-        it1++;
-    }
-
-    while (mid + it2 < right) {
-        result[it1 + it2] = array[mid + it2];
-        it2++;
-    }
-
-    for (size_t i = 0; i < it1 + it2; i++)
-        array[left + i] = result[i];
+    std::memcpy(&result[it1 + it2], &array[left + it1], (mid - left - it1) * sizeof(array[0]));
+    std::memcpy(&result[mid - left + it2], &array[mid + it2], (right - mid - it2) * sizeof(array[0]));
+    std::memcpy(&array[left], &result[0], result.size() * sizeof(T));
 }
 
 
@@ -57,8 +39,11 @@ void mergeSort(std::vector<T>& array) {
 template <class T>
 void mergeSortMT(std::vector<T>& array) {
     size_t mid = array.size() / 2;
-    std::vector<T> leftHalf = takeSequence(array, 0, mid);
-    std::vector<T> rightHalf = takeSequence(array, mid, array.size());
+    std::vector<T> leftHalf(mid);
+    std::vector<T> rightHalf(array.size() - mid);
+
+    std::memcpy(&leftHalf[0], &array[0], leftHalf.size() * sizeof(array[0]));
+    std::memcpy(&rightHalf[0], &array[mid], rightHalf.size() * sizeof(array[0]));
 
     std::thread sortLeft(mergeSort<T>, std::ref(leftHalf));
     std::thread sortRight(mergeSort<T>, std::ref(rightHalf));
@@ -77,8 +62,8 @@ class FileSort
 {
 public:
     FileSort(std::string inputPath, std::string outputPath,
-             const size_t chunkSize) : inputPath(std::move(inputPath)),
-                                       outputPath(std::move(outputPath)),
+             const size_t chunkSize) : inputPath_(std::move(inputPath)),
+                                       outputPath_(std::move(outputPath)),
                                        chunkSize(chunkSize) {}
 
     void startSorting() {
@@ -88,34 +73,30 @@ public:
 
 private:
 
-    void readChunkFromFileToVec(std::vector<T>& chunkData) {
-        size_t i = 0;
-        for (; (i < chunkData.size()) && !this->input.eof(); i++)
-            this->input.read(reinterpret_cast<char *>(&chunkData[i]),
-                             sizeof(chunkData[i]));
-        if (i != chunkData.size())
-            chunkData.resize(i-1);
+    void readChunkFromFileToVec(std::vector<T>& chunkData, std::ifstream& input) {
+        input.read(reinterpret_cast<char *>(&chunkData[0]),
+                   chunkData.size() * sizeof(chunkData[0]));
     }
 
-    void writeChunkFromVecToFile(std::vector<T>& chunkData) {
-        for (size_t i = 0; i < chunkData.size(); i++)
-            this->output.write(reinterpret_cast<const char *>(&chunkData[i]),
-                               sizeof(chunkData[i]));
+    void writeChunkFromVecToFile(std::vector<T>& chunkData,
+                                 const std::string& outputPathLocal) {
+        std::ofstream output(outputPathLocal, std::ios::binary);
+        output.write(reinterpret_cast<const char *>(&chunkData[0]),
+                     chunkData.size() * sizeof(chunkData[0]));
+        output.close();
     }
 
     void createSortedChunks() {
         std::vector<T> chunkData(this->chunkSize);
-        this->input.open(this->inputPath, std::ios::binary);
-        while (!this->input.eof()) {
-            readChunkFromFileToVec(chunkData);
+        std::ifstream input(this->inputPath_, std::ios::binary);
+        while (!input.eof()) {
+            readChunkFromFileToVec(chunkData, input);
             mergeSortMT(chunkData);
             std::string tmpName = std::tmpnam(nullptr);
-            this->output.open(tmpPath + tmpName, std::ios::binary);
-            writeChunkFromVecToFile(chunkData);
+            writeChunkFromVecToFile(chunkData, tmpPath + tmpName);
             this->sortedChunkFilenames.push(std::move(tmpName));
-            this->output.close();
         }
-        this->input.close();
+        input.close();
     }
 
     void mergeSortedChunks() {
@@ -124,16 +105,16 @@ private:
             this->sortedChunkFilenames.pop();
 
             std::ifstream candidate(tmpPath + candidateName, std::ios::binary);
-            this->output.open(this->outputPath, std::ios::binary);
+            std::ofstream output(this->outputPath_, std::ios::binary);
 
             T tmp;
             while (!candidate.eof()) {
                 candidate.read(reinterpret_cast<char *>(&tmp), sizeof(tmp));
-                this->output.write(reinterpret_cast<char *>(&tmp), sizeof(tmp));
+                output.write(reinterpret_cast<char *>(&tmp), sizeof(tmp));
             }
 
             candidate.close();
-            this->output.close();
+            output.close();
             std::remove((tmpPath + candidateName).c_str());
             return;
         }
@@ -147,9 +128,8 @@ private:
             std::ifstream candidate1(tmpPath + candidate1Name, std::ios::binary);
             std::ifstream candidate2(tmpPath + candidate2Name, std::ios::binary);
             std::string tmpName = std::tmpnam(nullptr);
-            this->output.open(tmpPath + tmpName, std::ios::binary);
 
-            mergeChunks(candidate1, candidate2);
+            mergeChunks(candidate1, candidate2, tmpPath + tmpName);
 
             this->sortedChunkFilenames.push(std::move(tmpName));
 
@@ -164,56 +144,55 @@ private:
 
         std::ifstream candidate1(tmpPath + candidate1Name, std::ios::binary);
         std::ifstream candidate2(tmpPath + candidate2Name, std::ios::binary);
-        this->output.open(this->outputPath, std::ios::binary);
 
-        mergeChunks(candidate1, candidate2);
+        mergeChunks(candidate1, candidate2, this->outputPath_);
 
         std::remove((tmpPath + candidate1Name).c_str());
         std::remove((tmpPath + candidate2Name).c_str());
     }
 
-    void mergeChunks(std::ifstream& candidate1, std::ifstream& candidate2) {
+    void mergeChunks(std::ifstream& candidate1, std::ifstream& candidate2,
+                     const std::string& outputPathLocal) {
         T value1;
         T value2;
+        std::ofstream output(outputPathLocal, std::ios::binary);
         candidate1.read(reinterpret_cast<char *>(&value1), sizeof(value1));
         candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
         while (!candidate1.eof() && !candidate2.eof()) {
             if (value1 < value2) {
-                this->output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
+                output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
                 candidate1.read(reinterpret_cast<char *>(&value1), sizeof(value1));
             } else {
-                this->output.write(reinterpret_cast<char *>(&value2), sizeof(value2));
+                output.write(reinterpret_cast<char *>(&value2), sizeof(value2));
                 candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
             }
         }
 
         while (!candidate1.eof()) {
-            this->output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
+            output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
             candidate1.read(reinterpret_cast<char *>(&value1), sizeof(value1));
         }
 
         while (!candidate2.eof()) {
-            this->output.write(reinterpret_cast<char *>(&value2), sizeof(value2));
+            output.write(reinterpret_cast<char *>(&value2), sizeof(value2));
             candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
         }
 
         candidate1.close();
         candidate2.close();
-        this->output.close();
+        output.close();
     }
 
     const std::string tmpPath = "/var";
-    const std::string inputPath;
-    const std::string outputPath;
+    const std::string inputPath_;
+    const std::string outputPath_;
     const size_t chunkSize;
-    std::ifstream input;
-    std::ofstream output;
     std::stack<std::string> sortedChunkFilenames;
 };
 
 int main()
 {
-    FileSort<uint64_t> fileSort("data", "result", 100000);
+    FileSort<uint64_t> fileSort("data.dat", "result.dat", 100000);
     fileSort.startSorting();
     return 0;
 }
