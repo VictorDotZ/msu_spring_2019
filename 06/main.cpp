29e5a95c06ce6a7990a42d8311f40761311f838c
Victor Zenin <super.cat3124@gmail.com>
2019-05-15 10:46:30 +0300

    add files validation

--- 06/main.cpp
+++ 06/main.cpp
@@ -71,6 +71,14 @@ public:
         mergeSortedChunks();
     }
 
+    ~FileSort() {
+        while (!this->sortedChunkFilenames.empty()) {
+            std::string fileName = this->sortedChunkFilenames.top();
+            this->sortedChunkFilenames.pop();
+            std::remove((tmpPath + fileName).c_str());
+        }
+    }
+
 private:
 
     void readChunkFromFileToVec(std::vector<T>& chunkData, std::ifstream& input) {
@@ -81,25 +89,44 @@ private:
     void writeChunkFromVecToFile(std::vector<T>& chunkData,
                                  const std::string& outputPathLocal) {
         std::ofstream output(outputPathLocal, std::ios::binary);
+        if (!output.is_open()) {
+            output.close();
+            this->~FileSort();
+            throw std::runtime_error("file open error");
+        }
         output.write(reinterpret_cast<const char *>(&chunkData[0]),
                      chunkData.size() * sizeof(chunkData[0]));
+        if (!output.good()) {
+            output.close();
+            this->~FileSort();
+            throw std::runtime_error("Write to disk error");
+        }
         output.close();
     }
 
     void createSortedChunks() {
         std::vector<T> chunkData(this->chunkSize);
         std::ifstream input(this->inputPath_, std::ios::binary | std::ios::ate);
+        if (!input.is_open()) {
+            input.close();
+            throw std::runtime_error("file open error");
+        }
+
         size_t fileLength = input.tellg() / sizeof(T);
         input.seekg(0);
-        while (!input.eof() && this->chunkSize == chunkData.size()) {
+        while (input.good() && this->chunkSize == chunkData.size()) {
             if (fileLength - input.tellg() / sizeof(T) < this->chunkSize)
                 chunkData.resize(fileLength - input.tellg() / sizeof(T));
-            std::cout << chunkData.size() << std::endl;
             readChunkFromFileToVec(chunkData, input);
             mergeSortMT(chunkData);
             std::string tmpName = std::tmpnam(nullptr);
-            writeChunkFromVecToFile(chunkData, tmpPath + tmpName);
             this->sortedChunkFilenames.push(std::move(tmpName));
+            writeChunkFromVecToFile(chunkData, tmpPath + this->sortedChunkFilenames.top());
+        }
+        if (!input.good()) {
+            input.close();
+            this->~FileSort();
+            throw std::runtime_error("Invalid input");
         }
         input.close();
     }
@@ -107,10 +134,16 @@ private:
     void mergeSortedChunks() {
         if (this->sortedChunkFilenames.size() == 1) {
             std::string candidateName = this->sortedChunkFilenames.top();
-            this->sortedChunkFilenames.pop();
 
             std::ifstream candidate(tmpPath + candidateName, std::ios::binary);
             std::ofstream output(this->outputPath_, std::ios::binary);
+
+            if (!candidate.is_open() || !output.is_open()) {
+                candidate.close();
+                output.close();
+                this->~FileSort();
+                throw std::runtime_error("process error");
+            }
 
             T tmp;
             while (!candidate.eof()) {
@@ -118,8 +151,16 @@ private:
                 output.write(reinterpret_cast<char *>(&tmp), sizeof(tmp));
             }
 
+            if (!candidate.eof()) {
+                candidate.close();
+                output.close();
+                this->~FileSort();
+                throw std::runtime_error("process error");
+            }
+
             candidate.close();
             output.close();
+            this->sortedChunkFilenames.pop();
             std::remove((tmpPath + candidateName).c_str());
             return;
         }
@@ -128,41 +169,58 @@ private:
             std::string candidate1Name = this->sortedChunkFilenames.top();
             this->sortedChunkFilenames.pop();
             std::string candidate2Name = this->sortedChunkFilenames.top();
-            this->sortedChunkFilenames.pop();
+            this->sortedChunkFilenames.push(candidate1Name);
 
-            std::ifstream candidate1(tmpPath + candidate1Name, std::ios::binary);
-            std::ifstream candidate2(tmpPath + candidate2Name, std::ios::binary);
             std::string tmpName = std::tmpnam(nullptr);
-
-            mergeChunks(candidate1, candidate2, tmpPath + tmpName);
-
             this->sortedChunkFilenames.push(std::move(tmpName));
 
+            mergeChunks(tmpPath + candidate1Name,
+                        tmpPath + candidate2Name,
+                        tmpPath + this->sortedChunkFilenames.top());
+
+            std::string saveTop = this->sortedChunkFilenames.top();
+            this->sortedChunkFilenames.pop();
             std::remove((tmpPath + candidate1Name).c_str());
+            this->sortedChunkFilenames.pop();
             std::remove((tmpPath + candidate2Name).c_str());
+            this->sortedChunkFilenames.pop();
+            this->sortedChunkFilenames.push(std::move(saveTop));
         }
 
         std::string candidate1Name = this->sortedChunkFilenames.top();
         this->sortedChunkFilenames.pop();
         std::string candidate2Name = this->sortedChunkFilenames.top();
-        this->sortedChunkFilenames.pop();
+        this->sortedChunkFilenames.push(candidate1Name);
 
-        std::ifstream candidate1(tmpPath + candidate1Name, std::ios::binary);
-        std::ifstream candidate2(tmpPath + candidate2Name, std::ios::binary);
-
-        mergeChunks(candidate1, candidate2, this->outputPath_);
+        mergeChunks(tmpPath + candidate1Name,
+                    tmpPath + candidate2Name,
+                    this->outputPath_);
 
         std::remove((tmpPath + candidate1Name).c_str());
+        this->sortedChunkFilenames.pop();
         std::remove((tmpPath + candidate2Name).c_str());
+        this->sortedChunkFilenames.pop();
     }
 
-    void mergeChunks(std::ifstream& candidate1, std::ifstream& candidate2,
+    void mergeChunks(const std::string& candidate1Name,
+                     const std::string& candidate2Name,
                      const std::string& outputPathLocal) {
         T value1;
         T value2;
+        std::ifstream candidate1(candidate1Name, std::ios::binary);
+        std::ifstream candidate2(candidate2Name, std::ios::binary);
         std::ofstream output(outputPathLocal, std::ios::binary);
+
+        if (!output.is_open() || !candidate1.is_open() || !candidate2.is_open()) {
+            candidate1.close();
+            candidate2.close();
+            output.close();
+            this->~FileSort();
+            throw std::runtime_error("File reading error");
+        }
         candidate1.read(reinterpret_cast<char *>(&value1), sizeof(value1));
         candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
+
         while (!candidate1.eof() && !candidate2.eof()) {
             if (value1 < value2) {
                 output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
@@ -172,15 +230,20 @@ private:
                 candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
             }
         }
-
         while (!candidate1.eof()) {
             output.write(reinterpret_cast<char *>(&value1), sizeof(value1));
             candidate1.read(reinterpret_cast<char *>(&value1), sizeof(value1));
         }
-
         while (!candidate2.eof()) {
             output.write(reinterpret_cast<char *>(&value2), sizeof(value2));
             candidate2.read(reinterpret_cast<char *>(&value2), sizeof(value2));
+        }
+        if (!output.good() || !candidate1.eof() || !candidate2.eof()) {
+            candidate1.close();
+            candidate2.close();
+            output.close();
+            this->~FileSort();
+            throw std::runtime_error("File merge error");
         }
 
         candidate1.close();
@@ -199,5 +262,6 @@ int main()
 {
     FileSort<uint64_t> fileSort("data.dat", "result.dat", 100000);
     fileSort.startSorting();
+
     return 0;
 }
